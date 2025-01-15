b46e08b947834d92362321837060547fb37a2349
John Jeffries <jeffries839@gmail.com>
2025-01-15 16:27:18 +0000

    post model created

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    authors = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_posts")
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    STATUS = ((0, "Draft"), (1, "published"))
