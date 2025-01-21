cb969f58be8e2cef13f3bb3cc934e128ad65fe59
NewWorldHost <jeffries839@gmail.com>
2025-01-21 16:01:58 +0000

    coder var add

--- blog/views.py
+++ blog/views.py
@@ -29,7 +29,8 @@ def post_detail(request, slug):
     return render(
         request,
         "blog/post_detail.html",
-        {"post": post},
+        {"post": post,
+        "coder": "Matt Rudge"},
     )
 def event_detail(request, event_id):
     
