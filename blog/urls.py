5ae67ac37f8daa9852a4ddc68678e0dd7ea37e0d
NewWorldHost <jeffries839@gmail.com>
2025-01-23 10:43:52 +0000

    delete function added

--- blog/urls.py
+++ blog/urls.py
@@ -7,5 +7,7 @@ from . import views
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
-    path("<int:event_id>/", views.event_detail, name="event_detail") 
+    path("<int:event_id>/", views.event_detail, name="event_detail"),
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>', views.comment_delete, name='comment_delete'),
 ]
